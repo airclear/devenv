d87e592c9e7e806005f4b7ba457b96dfe12ed945
lihang <lihang4work@gmail.com>
2017-09-28 17:33:11 +0800

    自动获取主机IP

--- nginx/bin/run-zaozuo.sh
+++ nginx/bin/run-zaozuo.sh
@@ -6,9 +6,25 @@ WEBROOT=/Users/airclear/works/projects/zaozuo/zaozuo-web/src/main/resources
 # linux获取ip
 # ifconfig|grep 'inet '|grep -v '127.0'|xargs|awk -F '[ :]' '{print $2}'
 
-cd ../ && docker run -d -p 80:80 -p 443:443 \
--v $PWD/:/data/nginx/ \
--v $PWD/conf/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro \
--v $PWD/logs:/usr/local/openresty/nginx/logs \
+localip=`ifconfig|grep 'inet '|grep -v '127.0'|xargs|awk -F '[ :]' '{print $2}'`
+
+# 获取当前脚本路径
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+
+rm -f ${DIR}/../conf/conf.d/zaozuo.conf
+cp ${DIR}/../conf/conf.d/zaozuo.conf.tmpl ${DIR}/../conf/conf.d/zaozuo.conf
+sed -i '' "s/hostip/${localip}/g" ${DIR}/../conf/conf.d/zaozuo.conf
+
+
+docker run -d -p 80:80 -p 443:443 \
+-v $DIR/../:/data/nginx/ \
+-v $DIR/../conf/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf:ro \
+-v $DIR/../logs:/usr/local/openresty/nginx/logs \
 -v $WEBROOT:/data/nginx/html/zaozuo/ \
 airclear/openresty
